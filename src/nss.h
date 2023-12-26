d82cce613f3f0ac95ba0720c20e15ba10cc2b963
firmwarecostum <145313986+firmwarecostum@users.noreply.github.com>
2023-12-26 14:50:34 +0800

    Add files via upload

--- src/nss.h
+++ src/nss.h
@@ -1,5 +1,6 @@
 #ifndef src_nss_h
 #define src_nss_h
+#endif
 
 #if defined(NSS_IPV4_ONLY) && !defined(MDNS_MINIMAL)
 #define _nss_mdns_gethostbyname4_r _nss_mdns4_gethostbyname4_r
@@ -31,22 +32,4 @@
 #define _nss_mdns_gethostbyname2_r _nss_mdns_minimal_gethostbyname2_r
 #define _nss_mdns_gethostbyname_r _nss_mdns_minimal_gethostbyname_r
 #define _nss_mdns_gethostbyaddr_r _nss_mdns_minimal_gethostbyaddr_r
-#endif
-
-// Define prototypes for nss function we're going to export (fixes GCC warnings)
-#ifndef __FreeBSD__
-enum nss_status _nss_mdns_gethostbyname4_r(const char*, struct gaih_addrtuple**,
-                                           char*, size_t, int*, int*, int32_t*);
-#endif
-enum nss_status _nss_mdns_gethostbyname3_r(const char*, int, struct hostent*,
-                                           char*, size_t, int*, int*, int32_t*,
-                                           char**);
-enum nss_status _nss_mdns_gethostbyname2_r(const char*, int, struct hostent*,
-                                           char*, size_t, int*, int*);
-enum nss_status _nss_mdns_gethostbyname_r(const char*, struct hostent*, char*,
-                                          size_t, int*, int*);
-enum nss_status _nss_mdns_gethostbyaddr_r(const void*, int, int,
-                                          struct hostent*, char*, size_t, int*,
-                                          int*);
-
 #endif
